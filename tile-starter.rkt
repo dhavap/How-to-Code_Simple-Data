c1293731150b1e3f77b0f3b570e9948901a3e38d
Dhava Palanivelu <50911475+dhavap@users.noreply.github.com>
2022-03-02 20:22:12 -0800

    create a checkered board

---
+++ tile-starter.rkt
@@ -0,0 +1,83 @@
+(require 2htdp/image)
+
+; 
+; PROBLEM:
+; 
+; Use the DrRacket square, beside and above functions to create an image like this one:
+; 
+; .
+; 
+; If you prefer to be more creative feel free to do so. You can use other DrRacket image 
+; functions to make a more interesting or more attractive image.
+; 
+
+
+(above
+ (beside
+  (above (square 10 "solid" "pink")
+         (square 10 "solid" "black"))
+  (above (square 10 "solid" "black")
+         (square 10 "solid" "pink"))
+  (above (square 10 "solid" "pink")
+         (square 10 "solid" "black"))
+  (above (square 10 "solid" "black")
+         (square 10 "solid" "pink"))
+  (above (square 10 "solid" "pink")
+         (square 10 "solid" "black"))
+  (above (square 10 "solid" "black")
+         (square 10 "solid" "pink"))
+  (above (square 10 "solid" "pink")
+         (square 10 "solid" "black"))
+  (above (square 10 "solid" "black")
+         (square 10 "solid" "pink")))
+ (beside
+       (above (square 10 "solid" "pink")
+              (square 10 "solid" "black"))
+       (above (square 10 "solid" "black")
+              (square 10 "solid" "pink"))
+       (above (square 10 "solid" "pink")
+              (square 10 "solid" "black"))
+       (above (square 10 "solid" "black")
+              (square 10 "solid" "pink"))
+       (above (square 10 "solid" "pink")
+              (square 10 "solid" "black"))
+       (above (square 10 "solid" "black")
+              (square 10 "solid" "pink"))
+       (above (square 10 "solid" "pink")
+              (square 10 "solid" "black"))
+       (above (square 10 "solid" "black")
+              (square 10 "solid" "pink")))
+ (beside
+       (above (square 10 "solid" "pink")
+              (square 10 "solid" "black"))
+       (above (square 10 "solid" "black")
+              (square 10 "solid" "pink"))
+       (above (square 10 "solid" "pink")
+              (square 10 "solid" "black"))
+       (above (square 10 "solid" "black")
+              (square 10 "solid" "pink"))
+       (above (square 10 "solid" "pink")
+              (square 10 "solid" "black"))
+       (above (square 10 "solid" "black")
+              (square 10 "solid" "pink"))
+       (above (square 10 "solid" "pink")
+              (square 10 "solid" "black"))
+       (above (square 10 "solid" "black")
+              (square 10 "solid" "pink")))
+ (beside
+       (above (square 10 "solid" "pink")
+              (square 10 "solid" "black"))
+       (above (square 10 "solid" "black")
+              (square 10 "solid" "pink"))
+       (above (square 10 "solid" "pink")
+              (square 10 "solid" "black"))
+       (above (square 10 "solid" "black")
+              (square 10 "solid" "pink"))
+       (above (square 10 "solid" "pink")
+              (square 10 "solid" "black"))
+       (above (square 10 "solid" "black")
+              (square 10 "solid" "pink"))
+       (above (square 10 "solid" "pink")
+              (square 10 "solid" "black"))
+       (above (square 10 "solid" "black")
+              (square 10 "solid" "pink"))))
