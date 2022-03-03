5220bbb12696f7beede16f94aaf10e7c791fc2ca
Dhava Palanivelu <50911475+dhavap@users.noreply.github.com>
2022-03-02 20:13:29 -0800

    solution to first arithmetic problem

---
+++ arithmetic-expressions-1.rkt
@@ -0,0 +1,13 @@
+; 
+; PROBLEM:
+; 
+; Write two expressions that multiply the numbers 3, 5 and 7. 
+; The first should take advantage of the fact that * can accept more than 2 arguments. 
+; The second should build up the result by first multiplying 3 times 5 and then multiply the result of that by 7. 
+; 
+
+; Solution 1
+(* 3 5 7)
+
+; Solution 2
+(* (* 3 5) 7)
